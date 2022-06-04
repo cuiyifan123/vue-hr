5bc191cf5baaf051db278d8d842b6fa20db742b5
Geek <3346757547@qq.com>
2022-06-04 10:38:55 +0800

    'fix：修复公司设置，表格数据有问题的bug'

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ const service = axios.create({
   timeout: 5000 // 超时处理
 })
 
-service.interceptors.request.use(config => {
+service.interceptors.request.use((config) => {
   const { url } = config
   if (url.startsWith('/api/sys/login')) return config
   const token = store.getters.token
@@ -17,25 +17,29 @@ service.interceptors.request.use(config => {
   return config
 })
 
-service.interceptors.response.use((response) => {
-  // 如果success是false表示响应成功了，
-  // 但是success为false在业务逻辑上是错误的，不是我们想要的结果，比如：账号密码错误。
-  // 所以直接return 一个错误的promise，让发请求那里直接报错(请求那里只处理成功的情况)
-  if (!response.data.success) {
-    Message.error({
-      message: response.data.message,
-      duration: 2000
-    })
-    return Promise.reject(new Error(response.data.message))
+service.interceptors.response.use(
+  (response) => {
+    // 如果success是false表示响应成功了，
+    // 但是success为false在业务逻辑上是错误的，不是我们想要的结果，比如：账号密码错误。
+    // 所以直接return 一个错误的promise，让发请求那里直接报错(请求那里只处理成功的情况)
+    if (!response.data.success) {
+      Message.error({
+        message: response.data.message,
+        duration: 2000
+      })
+      return Promise.reject(new Error(response.data.message))
+    }
+    return response.data
+  },
+  async(reason) => {
+    // 如果状态码为401，表示token过期了，重定向到登录页
+    if (reason.response?.status === 401) {
+      await store.dispatch({ type: 'user/userLogout', payload: null })
+      // 但是页面刷新是在根页面的，导致传递过去的from是 /首页，所以需要获取历史记录传递给登录页
+      const redirect = location.href.replace(/.+#/g, '')
+      router.push({ path: '/login', query: { redirect }})
+    }
+    return Promise.reject(reason)
   }
-  return response.data
-}, async(reason) => {
-  // 如果状态码为401，表示token过期了，重定向到登录页
-  if (reason.response?.status === 401) {
-    await store.dispatch({ type: 'user/userLogout', payload: null })
-    // 但是页面刷新是在根页面的，导致传递过去的from是 /首页，所以需要获取历史记录传递给登录页
-    const redirect = location.href.replace(/.+#/g, '')
-    router.push({ path: '/login', query: { redirect }})
-  }
-})
+)
 export default service
--- src/views/settings/settings.vue
+++ src/views/settings/settings.vue
@@ -6,7 +6,7 @@
           <!-- 放置页签 -->
           <el-tab-pane label="角色管理">
             <!-- 新增角色按钮 -->
-            <el-row style="height:60px">
+            <el-row style="height: 60px">
               <el-button
                 icon="el-icon-plus"
                 size="small"
@@ -16,29 +16,37 @@
             </el-row>
             <!-- 表格 -->
             <el-table :data="rows" border>
-              <el-table-column label="序号" width="120" type="index"/>
-              <el-table-column label="角色名称" width="240" prop="name"/>
-              <el-table-column label="描述" prop="description"/>
+              <el-table-column label="序号" width="120" type="index" />
+              <el-table-column label="角色名称" width="240" prop="name" />
+              <el-table-column label="描述" prop="description" />
               <el-table-column label="操作" width="300">
-                <template v-slot="{row}">
+                <template v-slot="{ row }">
                   <el-button size="small" type="success">分配权限</el-button>
                   <el-button size="small" type="primary">编辑</el-button>
-                  <el-button size="small" type="danger" @click="removeRole(row)">删除</el-button>
+                  <el-button
+                    size="small"
+                    type="danger"
+                    @click="removeRole(row)"
+                  >删除</el-button>
                 </template>
               </el-table-column>
             </el-table>
-            <el-row type="flex" justify="center" align="middle" style="height: 60px">
+            <el-row
+              type="flex"
+              justify="center"
+              align="middle"
+              style="height: 60px"
+            >
               <!-- 分页组件 -->
               <el-pagination
-                @size-change="handleSizeChange"
-                @current-change="handleCurrentChange"
                 :current-page="pageParams.page"
-                :page-sizes="[2, 3,5, 10]"
+                :page-sizes="[2, 3, 5, 10]"
                 :page-size="pageParams.pagesize"
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="total"
-              >
-              </el-pagination>
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+              />
             </el-row>
           </el-tab-pane>
         </el-tabs>
@@ -83,6 +91,11 @@ export default {
       this.$confirm('真的要删除吗?', '提示', { type: 'success' })
         .then(async() => {
           try {
+            // 判断当前分页，是否只有一条数据并且当前不在第一页，如果是则让下次请求页面 - 1
+            const { pageParams, rows } = this
+            if (pageParams.page > 1 && rows.length === 1) {
+              this.pageParams.page = pageParams.page - 1
+            }
             const res = await removeRole(id)
             // 成功提示用户
             this.$message.success(res.message)
@@ -92,8 +105,7 @@ export default {
             this.$message.error(e.message)
           }
         })
-        .catch(() => {
-        })
+        .catch(() => {})
     }
   }
 }
